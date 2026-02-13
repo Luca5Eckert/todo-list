96c1937893a69450099b7305153ec882dffa2872
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-02-13 15:14:01 -0300

    feat: add Todo entity

---
+++ clean-code/src/main/java/br/com/senai/centroWeg/domain/Todo.java
@@ -0,0 +1,48 @@
+package br.com.senai.centroWeg.domain;
+
+public class Todo {
+
+    private final int id;
+
+    private String title;
+
+    private String description;
+
+    private StatusTodo statusTodo;
+
+    public Todo(int id, String title, String description, StatusTodo statusTodo) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.statusTodo = statusTodo;
+    }
+
+    public StatusTodo getStatusTodo() {
+        return statusTodo;
+    }
+
+    public void setStatusTodo(StatusTodo statusTodo) {
+        this.statusTodo = statusTodo;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+}
