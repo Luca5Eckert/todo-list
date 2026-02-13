bf57f030007ff0f885706528c9990ea5fdbb743a
heloo86 <heloisa.af86@gmail.com>
2026-02-13 15:26:56 -0300

    chore: metodos para tarefa

--- bad-code/src/main/java/br/com/senai/centroWeg/service/TarefaService.java
+++ bad-code/src/main/java/br/com/senai/centroWeg/service/TarefaService.java
@@ -1,4 +1,20 @@
 package br.com.senai.centroWeg.service;
 
+import br.com.senai.centroWeg.model.Tarefa;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TarefaService {
+
+    public static final List<Tarefa> tarefas = new ArrayList<>();
+
+    public void alterarStatus(){}
+
+    public void excluirTarefa(){}
+
+    public void criarTarefa(){}
+
+    public void verTarefa(){}
+
 }
