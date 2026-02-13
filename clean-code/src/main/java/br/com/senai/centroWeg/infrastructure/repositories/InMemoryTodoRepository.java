2efb22b55c4c00a48193b2e8b04a9da90559089a
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-02-13 15:26:09 -0300

    feat: add TodoRepository

---
+++ clean-code/src/main/java/br/com/senai/centroWeg/infrastructure/repositories/InMemoryTodoRepository.java
@@ -0,0 +1,6 @@
+package br.com.senai.centroWeg.infrastructure.repositories;
+
+import br.com.senai.centroWeg.repository.TodoRepository;
+
+public class InMemoryTodoRepository implements TodoRepository {
+}
