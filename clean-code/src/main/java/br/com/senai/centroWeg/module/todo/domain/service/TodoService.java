87faffcb2fb30445889d7118db811bccd5d78fb1
heloo86 <heloisa.af86@gmail.com>
2026-02-20 16:43:33 -0300

    chore: Adiciona comentarios no todo service

--- clean-code/src/main/java/br/com/senai/centroWeg/module/todo/domain/service/TodoService.java
+++ clean-code/src/main/java/br/com/senai/centroWeg/module/todo/domain/service/TodoService.java
@@ -1,25 +1,32 @@
-package br.com.senai.centroWeg.module.todo.domain.service;
+package br.com.senai.centroWeg.module.todo.service;
 
-import br.com.senai.centroWeg.module.todo.domain.model.Todo;
-import br.com.senai.centroWeg.module.todo.application.dto.TodoUpdateRequest;
-import br.com.senai.centroWeg.module.todo.domain.repository.TodoRepository;
+import br.com.senai.centroWeg.module.todo.model.Todo;
+import br.com.senai.centroWeg.module.todo.dto.TodoUpdateRequest;
+import br.com.senai.centroWeg.module.todo.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class TodoService {
 
+    @Autowired
     private final TodoRepository todoRepository;
 
     public TodoService(TodoRepository todoRepository) {
         this.todoRepository = todoRepository;
     }
 
-    public void create (TodoUpdateRequest request){
+    public Todo create (TodoUpdateRequest request){
+
+        //Valida se o titulo é unico
         if(todoRepository.existsByTitle(request.title())){
             throw new RuntimeException("Já exite um item com este titulo");
         }
 
+        //Converte o request em entidade
         Todo todo = new Todo(
                 request.title(),
                 request.Description(),
@@ -28,6 +35,7 @@ public class TodoService {
         );
 
         todoRepository.save(todo);
+        return todo;
     }
 
     public void update(int todoId, TodoUpdateRequest request){
@@ -40,7 +48,7 @@ public class TodoService {
     }
 
     public Optional<Todo> findById (int id){
-       return todoRepository.findById(id);
+        return todoRepository.findById(id);
     }
 
 
