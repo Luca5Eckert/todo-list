c455882a5f670e5f1c9ee5f6a0ae72ac8bcb05a7
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-02-20 14:44:10 -0300

    feat: adiciona configurações nos controllers

--- clean-code/src/main/java/br/com/senai/centroWeg/module/todo/controller/TodoController.java
+++ clean-code/src/main/java/br/com/senai/centroWeg/module/todo/controller/TodoController.java
@@ -1,5 +1,10 @@
 package br.com.senai.centroWeg.module.todo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/todos")
 public class TodoController {
 
 
