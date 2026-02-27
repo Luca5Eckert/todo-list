a6e27e1d97afa89f9d8829cfb853bc077a0ac506
LorhanPierre <lorhanpierre11102007@gmail.com>
2026-02-27 15:40:19 -0300

    feat: cria crud de user (#3)

--- clean-code/src/main/java/br/com/senai/centroWeg/module/streak/domain/query/GetStreakAnalyticsQuery.java
+++ clean-code/src/main/java/br/com/senai/centroWeg/module/streak/domain/query/GetStreakAnalyticsQuery.java
@@ -2,6 +2,7 @@ package br.com.senai.centroWeg.module.streak.domain.query;
 
 public record GetStreakAnalyticsQuery(int userId) {
     public static GetStreakAnalyticsQuery of(int userId) {
+
         return new GetStreakAnalyticsQuery(userId);
     }
 }
