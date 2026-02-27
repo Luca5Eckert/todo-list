d9910da7332270594d4916ade05d6afce032d966
LorhanPierre <lorhanpierre11102007@gmail.com>
2026-02-27 15:38:28 -0300

    feat: cria crud de user

--- clean-code/src/main/java/br/com/senai/centroWeg/module/user/domain/repository/UserRepository.java
+++ clean-code/src/main/java/br/com/senai/centroWeg/module/user/domain/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package br.com.senai.centroWeg.module.user.domain.repository;
 
 import br.com.senai.centroWeg.module.user.domain.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,6 +11,7 @@ public interface UserRepository  {
 
     User save(User todo);
     List<User> findAll();
-    User findById(int Id);
-
+    Optional<User> findById(int Id);
+    User save(int id,User user);
+    void delete(int id);
 }
